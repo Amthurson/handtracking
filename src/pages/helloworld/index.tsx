a7110c3ea6d6b7f17e37d937bd93e3d7283d63f5
Amthurson <463793068@qq.com>
2021-09-07 21:00:53 +0800

    update(pages): 1. finished the first stage, capture the camera frame and detected the face and hands by handtrackingjs

--- src/pages/helloworld/index.tsx
+++ src/pages/helloworld/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './index.scss'
 
 // init handtrack
@@ -17,38 +17,66 @@ const defaultParams = {
   fontSize: 17,
 };
 
-// const model = await handTrack.load(defaultParams)
+const model = await handTrack.load(defaultParams)
 import Handimg from "./hand.png";
 
-const HelloWorld = () => {
-  const [video, setVideo] = useState();
+let isVideo = false;
+
+const HelloWorld = React.forwardRef(() => {
+  const vidoeDom = useRef(null)
+  const canvas = useRef(null)
+
+  // const [video, setVideo] = useState<HTMLElement | null>(null);
+  // const [isVideo, setIsVideo] = useState(false);
 
   const StartVideo = async () => {
-    const video = document. getElementById('video');
+    // const video = document. getElementById('video');
+    // console.log(video);
+    const video = vidoeDom.current
+    // setVideo(video);
+    console.log(video);
     const img = document.getElementById('handimg');
-    const model =  await handTrack.load(defaultParams);
-    const predictions = await model.detect(img); 
-    console.log(predictions);
     handTrack.startVideo(video);
+    isVideo = true
+    runDetection();
   }
 
   const StopVideo = async () => {
+    const video = vidoeDom.current
     handTrack.stopVideo(video);
+    isVideo = false
+  }
+
+  const runDetection = async () => {
+    // const canvas = document.getElementById("canvas") as HTMLElement;
+    const video = vidoeDom.current
+    const canvasDom = canvas.current;
+    const context = canvasDom.getContext("2d");
+    const model =  await handTrack.load(defaultParams);
+    model.detect(video).then((predictions: any) => {
+      console.log("Predictions: ", predictions);
+      model.renderPredictions(predictions, canvasDom, context, video);
+      console.log(isVideo)
+      if (isVideo) {
+        requestAnimationFrame(runDetection);
+      }
+    });
   }
 
   return (
     <div className="App">
-      <div className="Label">HelloWorld</div>
+      <div className="Label">This is deme of handTrackingjs Using In Vite React Programmes</div>
       <div className="control-wrap">
         <div className="btn-start" onClick={()=>StartVideo()}>Start Video</div>
         <div className="btn-stop" onClick={()=>StopVideo()}>Stop Video</div>
       </div>
       <div className="VideoContainer">
         <img src={Handimg} id="handimg" className="handimg" alt="" />
-        <video className="video" style={{height: "30vh !important"}} src="" id="video"></video>
+        <video ref={vidoeDom} className="video" style={{height: "30vh !important"}} src="" id="video"></video>
+        <canvas ref={canvas} id="canvas" className="border canvasbox"></canvas>
       </div>
     </div>
   )
-}
+})
 
 export default HelloWorld
